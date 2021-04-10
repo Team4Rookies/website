430bd7b0f309f44eb0a4934f32cdca9e1e0d119b
Abdallah Abedraba <aabedraba@gmail.com>
2021-04-10 18:41:20 +0200

    Convert files to typescript files

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-const Index = () => {
-  return <h1>Team 4 Rookies</h1>;
-};
-
-export default Index;
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header";
+
+const Index = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Team 4 Rookies</h1>
+    </div>
+  );
+};
+
+export default Index;
