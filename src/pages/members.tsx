30316d4bdffa4e648a26b8fb9eb458fb67f6f840
Abdallah Abedraba <aabedraba@gmail.com>
2021-04-10 20:00:32 +0200

    Read and display members data from folder

--- src/pages/members.tsx
+++ src/pages/members.tsx
@@ -1,12 +1,74 @@
+import fs from "fs";
+import { GetServerSideProps } from "next";
 import { Header } from "../components/Header";
 
-const Members = () => {
+type MemberData = {
+  about: string;
+  name: string;
+  social: {
+    twitter?: string;
+  };
+  slug: string;
+  hasPicture: boolean;
+};
+
+type MembersPage = {
+  membersData: MemberData[];
+};
+
+const Members = ({ membersData }: MembersPage) => {
   return (
     <div>
       <Header />
-      <h1>Members</h1>
+      <div>
+        {membersData.map((member) => {
+          return (
+            <div>
+              <h2>{member.name}</h2>
+              {member.hasPicture && (
+                <img
+                  src={`https://github.com/Team4Rookies/website/raw/main/teams-info/${member.slug}/pic.png`}
+                />
+              )}
+              <h3>About</h3>
+              <p>{member.about}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps<MembersPage> = async (
+  context
+) => {
+  const path = process.cwd() + "/teams-info/";
+  const members = fs.readdirSync(path);
+
+  const membersData: MemberData[] = [];
+
+  members.map((member) => {
+    const memberPath = path + "/" + member;
+    const memberFiles = fs.readdirSync(memberPath);
+    if (!memberFiles.includes("info.json")) {
+      return;
+    }
+
+    const data: MemberData = JSON.parse(
+      fs.readFileSync(memberPath + "/info.json", "utf-8")
+    );
+    data.slug = member;
+    data.hasPicture = memberFiles.includes("pic.png");
+
+    membersData.push(data);
+  });
+
+  return {
+    props: {
+      membersData,
+    },
+  };
 };
 
 export default Members;
